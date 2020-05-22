6f3b4c58d7ba9811eba0d6fa64d18f7dd30f1856
Tom-Li1 <56505901+Tom-Li1@users.noreply.github.com>
2020-05-22 14:46:14 +0800

    yxh

--- python_files/exercise_example/pygame/spiritAndSounds/spritesAndSounds.py
+++ python_files/exercise_example/pygame/spiritAndSounds/spritesAndSounds.py
@@ -3,9 +3,9 @@ from pygame.locals import *
 
 pygame.init()
 mainClock = pygame.time.Clock()
-WINDOWWIDTH = 400
-WINDOWHEIGHT = 400
-windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWWIDTH), 0, 32)
+WINDOWWIDTH = 1920
+WINDOWHEIGHT = 1080
+windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), FULLSCREEN, 32)
 pygame.display.set_caption("Sprites and Sounds")
 
 WHITE = (255, 255, 255)
